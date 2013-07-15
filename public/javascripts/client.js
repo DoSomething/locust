3a3ace2cf8d410308cd7e6a98f59459eb0d55c33
blarin <ianguerin@gmail.com>
2013-07-15 13:07:22 -0400

    client js git marks removed

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 	var socket = io.connect(window.location.hostname);
-<<<<<<< HEAD
 	socket.on("ping", function (body) {
 		//console.log(body);
 	});
-=======
 	socket.on("ping", function (date, name) {
 		var endDate = Date.parse(date);
 		var diff = Date.today().getDaysBetween(endDate);
 		console.log(diff + " Days Left of " + name);
 	})
->>>>>>> 8647e251ab42756a444e462c10a771f1baca8e79
 });
