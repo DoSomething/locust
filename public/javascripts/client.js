86465fed420d7e7558b0d9f436071e4fbfbf90d2
ahojsak <allison_hojsak@brown.edu>
2013-07-15 15:08:27 -0400

    flightboard start

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	var socket = io.connect(window.location.hostname);
-	socket.on("ping", function (body) {
-		//console.log(body);
-	});
 	socket.on("ping", function (date, name) {
 		var endDate = Date.parse(date);
 		var diff = Date.today().getDaysBetween(endDate);
 		console.log(diff + " Days Left of " + name);
 	})
 });
+
+$('#featured').flightboard({maxLength: 14, 
+    messages: ['FIRST MESSAGE', 'SECOND MESSAGE', 'THIRD MESSAGE']});
